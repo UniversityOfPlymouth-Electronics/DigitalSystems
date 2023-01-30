b8ba17f87711f093bdb33a567a61f3cc011d2fb2
Macaw21 <90797789+Macaw21@users.noreply.github.com>
2023-01-30 15:46:35 +0000

    Update mux2_structural.sv

--- tasks/Task215-2-to-1-multiplexer/mux2_structural.sv
+++ tasks/Task215-2-to-1-multiplexer/mux2_structural.sv
@@ -1,6 +1,37 @@
 module mux2_structural (output logic y, input logic a, b, s);
 
+/*
+|  S  |  A  |  B  |  Y  |	Expression		|
+|-------------------------------------------------------|
+|  0  | 0  |  0	  |  0  |				|
+|  0  |	0  |  1	  |  0  |				|
+|  0  |	1  |  0	  |  1  |	~S & A & ~B		|
+|  0  |	1  |  1	  |  1  |	~S & A & B		|
+|  1  |	0  |  0	  |  0  |				|
+|  1  |	0  |  1	  |  1  |	S & ~A & B		|
+|  1  |	1  |  0	  |  0  |				|
+|  1  |	1  |  1	  |  1  |	S & A & B		|
+
+Expression for Y
+
+Y = (~S & A & ~B) | (~S & A & B) | (S & ~A & B) | (S & A & B)
+
+Simplified
+
+Y = (~S & A) | (S & B)
+
+*/
+
 // Write HDL here
 
+//internal wires
+wire invS;
+wire term_0;
+wire term_1;
+
+not u1 (invS, s);
+and u2 (term_0, a, b);
+and u3 (term_1, invS, a);
+or u4 (y, term_0, term_1);
 
 endmodule
